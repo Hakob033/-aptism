5bcb2145c658a68f9c4fe1cef7d3640467e197f1
karapetyanhakob4 <karapetyanhakob4@gmail.com>
2025-06-16 00:24:37 +0400

    change header stiles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,13 +4,13 @@ import Timer from "./timer";
 const Header = () => {
   return (
     <div
-      className="h-screen bg-fixed bg-center bg-cover relative"
+      className="h-[80vh] sm:h-screen bg-fixed bg-cover bg-center relative"
       style={{
         backgroundImage: 'url("/images/main-image.jpg")',
       }}
     >
-      <div className="absolute inset-0 bg-black/30 brightness-75 z-0"></div>
-      <div className="relative z-10 flex justify-center items-center h-full">
+      <div className="absolute inset-0 bg-black/30 brightness-75 z-0" />
+      <div className="relative z-10 flex justify-center items-center h-full px-4 sm:px-6">
         <Timer />
       </div>
     </div>
